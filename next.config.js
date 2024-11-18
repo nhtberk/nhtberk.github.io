4b57edf8c4fbfd14f25a65bf13933f682a52f679
Nihat Berk ÖZ <berkoz5555@gmail.com>
2024-11-18 20:09:05 +0300

    asdasdas

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/nhtberk.github.io', // GitHub repo adınızı buraya yazın
+}
+
+module.exports = nextConfig 
