f0fdace9dd6abcb1da0eab34881fa9b14d5938fc
Nihat Berk ÖZ <berkoz5555@gmail.com>
2024-11-18 19:28:53 +0300

    asdasdas

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // <=== enables static exports
   reactStrictMode: true,
+  eslint:{
+    ignoreDuringBuilds:true
+  }
   /* config options here */
 };
 
