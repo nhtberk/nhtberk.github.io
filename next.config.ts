4ccef687ebe34bee77cbf2b5ade9cbf84446a088
Nihat Berk ÖZ <berkoz5555@gmail.com>
2024-11-18 19:14:44 +0300

    asa

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/nextjs-github-pages",  // <=== enables static exports
+  output: "export", // <=== enables static exports
   reactStrictMode: true,
   /* config options here */
 };
