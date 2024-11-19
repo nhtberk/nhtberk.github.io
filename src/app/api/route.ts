c701ac9af35b0c42b78b0adc5af2f77207b9f8ab
n.berkoez@rtk-deutschland.de <n.berkoez@rtk-deutschland.de>
2024-11-19 11:50:04 +0300

    asdasda

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST(request: Request) {
+export default async function POST(request: Request) {
     console.log(1);
     
     try {
