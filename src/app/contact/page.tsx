6fa4b81a2526d1ec31df4820c07bcdf9e96615cf
n.berkoez@rtk-deutschland.de <n.berkoez@rtk-deutschland.de>
2024-11-19 11:56:05 +0300

    sdasdas

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -21,7 +21,7 @@ export default function Contact() {
     setSuccess(false)
 
     try {
-      const response = await fetch('/api/route', {
+      const response = await fetch('/api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
